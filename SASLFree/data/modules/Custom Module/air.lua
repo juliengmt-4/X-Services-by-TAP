c8cd4f772c49bf4c7ab08a608a9824f66e4be003
TheArtfulPlonker <59488892+TheArtfulPlonker@users.noreply.github.com>
2021-01-18 17:57:08 +0000

    Delete air.lua

--- SASLFree/data/modules/Custom Module/air.lua
+++
@@ -1,8 +0,0 @@
---A220 By TAP Designs--
---DATAREF CREATION--
-
-Window_Heat_L_Side = createGlobalPropertyi( "TAP/heat/Window_Heat_L_Side", 0)
-Window_Heat_R_Side = createGlobalPropertyi( "TAP/heat/Window_Heat_R_Side", 0)
-Winshld_Heat_L_Side = createGlobalPropertyi( "TAP/heat/Winshld_Heat_L_Side", 0)
-Winshld_Heat_R_Side = createGlobalPropertyi( "TAP/heat/Winshld_Heat_R_Side", 0)
-Probe_Heat = createGlobalPropertyi( "TAP/heat/Probe_Heat", 0)
