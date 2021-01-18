3b2bdb1e2bdebac6ba320d2cd69a83b799e35cb3
TheArtfulPlonker <59488892+TheArtfulPlonker@users.noreply.github.com>
2021-01-18 17:57:01 +0000

    Delete anti_ice.lua

--- SASLFree/data/modules/Custom Module/anti_ice.lua
+++
@@ -1,6 +0,0 @@
---A220 By TAP Designs--
---DATAREF CREATION--
-
-L_Cowl = createGlobalPropertyi("TAP/Anti_Ice/L_Cowl", 0)
-R_Cowl = createGlobalPropertyi("TAP/Anti_Ice/R_Cowl", 0)
-Wing = createGlobalPropertyi("TAP/Anti_Ice/Wing", 0)
