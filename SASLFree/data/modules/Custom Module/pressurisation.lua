123d0cd24dd5d5f542d609f167f1fa31ac3afcbb
TheArtfulPlonker <59488892+TheArtfulPlonker@users.noreply.github.com>
2021-01-18 17:55:58 +0000

    Delete pressurisation.lua

--- SASLFree/data/modules/Custom Module/pressurisation.lua
+++
@@ -1,8 +0,0 @@
---A220 By TAP Designs--
---DATAREF CREATION--
-
-Emer_Depress = createGlobalPropertyi("TAP/pressurisation/Emer_Depress", 0)
-Auto_Press = createGlobalPropertyi("TAP/pressurisation/Auto_Press", 0)
-Ditching = createGlobalPropertyi("TAP/pressurisation/Ditching", 0)
-Pax_Oxy = createGlobalPropertyi("TAP/pressurisation/Pax_Oxy", 0)
-Man_Rate = createGlobalPropertyi("TAP/pressurisation/Man_Rate", 50)
