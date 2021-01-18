27b99bc87d72824c605fd9e0b802477d882b9fc3
TheArtfulPlonker <59488892+TheArtfulPlonker@users.noreply.github.com>
2021-01-18 17:56:33 +0000

    Delete fuel.lua

--- SASLFree/data/modules/Custom Module/fuel.lua
+++
@@ -1,8 +0,0 @@
---A220 By TAP Designs--
---DATAREF CREATION--
-
-L_Boost_Pump = createGlobalPropertyi("TAP/Fuel/L_Boost_Pump", 0)
-R_Boost_Pump = createGlobalPropertyi("TAP/Fuel/R_Boost_Pump", 0)
-Man_Xfr = createGlobalPropertyi("TAP/Fuel/Man_Xfr", 0)
-Gray_Xfr = createGlobalPropertyi("TAP/Fuel/Gray_Xfr", 0)
-
